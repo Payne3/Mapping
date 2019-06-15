26e3a1712276530d1a040b40628e76e26f842952
Will Payne <46658669+Payne3@users.noreply.github.com>
2019-06-15 14:21:54 -0500

    Add files via upload

--- logic.js
+++ logic.js
@@ -8,24 +8,36 @@
 
 // Adding tile layer
 var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.streets",
-    accessToken: API_KEY
-  });
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+});
 
 
 var URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
 
+var geojsonMarkerOptions = {
+  radius: 8,
+  fillColor: "#ff7800",
+  color: "#000",
+  weight: 1,
+  opacity: 1,
+  fillOpacity: 0.8
+};
+
 d3.json(URL, function(data) {
-    // Creating a GeoJSON layer with the retrieved data
-    L.geoJson(data).addTo(map);
+  // Creating a GeoJSON layer with the retrieved data
+  L.geoJson(data, {
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+    }).addTo(map);
   });
 
-  // Creating map object
+// Creating map object
 var map = L.map("map-id", {
-    center: [40.7128, -74.0059],
-    zoom: 11,
-    layers: [streetmap]
-  });
-  
+  center: [40.7128, -74.0059],
+  zoom: 11,
+  layers: [streetmap]
+});
