c2733480fc31f721e02a7532b3057ad7b001223a
Will Payne <46658669+Payne3@users.noreply.github.com>
2019-06-17 15:57:16 -0500

    cleaned up logic file

--- logic.js
+++ logic.js
@@ -1,9 +1,4 @@
-// Creating map object
-// var map = L.map("map-id", {
-//   center: [40.7128, -74.0059],
-//   zoom: 11,
-//   layers: [streetmap]
-// });
+
 
 
 // Adding tile layer
@@ -17,40 +12,14 @@ var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.pn
 
 var URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
 
-// var geojsonMarkerOptions = {
-//   radius: markerSize(feature.properties.mag),
-//   fillColor: "#ff7800",
-//   color: "#000",
-//   weight: 1,
-//   opacity: 1,
-//   fillOpacity: 0.8
-// };
+
 
 function markerSize(magnitude) {
   return magnitude *5;
 };
 
 
-// d3.json(URL, function (data) {
-//   // Creating a GeoJSON layer with the retrieved data
-//   L.geoJson(data, {
-//     pointToLayer: function (feature, latlng) {
-//       return L.circleMarker(latlng);
-//     }
-//     }).addTo(map);
-// });
 
-// function markerOptions(options) {
-
-//   var sizing = L.geoJson(options, {
-//     style : function(feature) {
-//       return {
-//         fillColor: chooseColor(feature.property.mag),
-//         radius: feature.property.mag,
-//       }
-//     }
-//   })
-// }; markerOptions(sizing);
 
 d3.json(URL, function (data) {
   // Creating a GeoJSON layer with the retrieved data
